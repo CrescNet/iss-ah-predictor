bb50bc16d928f5b4f679f439d61c21a906b6d8a5
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-08-02 15:47:30 +0200

    Add placeholder perl module

---
+++ lib/ISS/AH/Predictor.pm
@@ -0,0 +1,26 @@
+package ISS::AH::Predictor;
+
+use v5.10;
+
+=head1 NAME
+
+ISS::AH::Predictor - Provides prediction models for adult height (AH) in patients with idiopathic short stature (ISS).
+
+=cut
+
+our $VERSION = '0.1.0';
+
+
+=head1 SYNOPSIS
+
+    use ISS::AH::Predictor;
+
+=head1 ATTRIBUTES
+
+=head1 SUBROUTINES/METHODS
+
+=head1 AUTHOR
+
+Christoph Beger, C<< <christoph.beger at medizin.uni-leipzig.de> >>
+
+=cut
